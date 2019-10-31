8a38d2d0fd4dc0bc49fa79b39d17824a2c9fbdd7
Adib <3923297-igdoma@users.noreply.gitlab.com>
2019-10-31 11:11:21 +0700

    ubah ukuran

--- src/main/java/pemilukm/teti/PopupView.java
+++ src/main/java/pemilukm/teti/PopupView.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import pemilukm.teti.controls.helpers.ControlsHelper;
 
@@ -30,8 +31,9 @@ class PopupView extends VBox {
 
 		AdminView adminView = new AdminView();
 		ControlsHelper.changeScene(adminView.content);
-//        GlobalVar.primaryStage.setWidth(600);
-//        GlobalVar.primaryStage.setHeight(400);
+        GlobalVar.primaryStage.setWidth(600);
+        GlobalVar.primaryStage.setHeight(400);
+        GlobalVar.primaryStage.initModality(Modality.APPLICATION_MODAL);
 		GlobalVar.primaryStage.show();
 		stage.close();
 
